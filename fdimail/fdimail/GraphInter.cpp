4bb5b77c6d2732ea20c59fb6875040b48e899979
vigonz06 <victorglez96@gmail.com>
2015-04-13 17:47:04 +0200

    placeholders
    stil does not work

--- fdimail/fdimail/GraphInter.cpp
+++ fdimail/fdimail/GraphInter.cpp
@@ -20,3 +20,57 @@ void GraphInter::load()
 {
 	if (inter = nullptr) inter = new GraphInter;
 }
+
+void drawMail(const Mail* mail)
+{
+
+}
+
+void drawTraylist(TrayList* list)
+{
+
+}
+
+void drawHeader(const Mail* mail)
+{
+
+}
+
+void showText(std::string &text)
+{
+
+}
+
+int logMenu()
+{
+	return 0;
+}
+
+int mainMenu()
+{
+	return 0;
+}
+
+bool mailMenu()
+{
+	return true;
+}
+
+std::string& inputBox(std::string &message)
+{
+	std::string var;
+
+	return var;//Placeholder
+}
+
+std::string& selectMail(TrayList* tray)
+{
+	std::string var;
+
+	return var;//Placeholder
+}
+
+void pause()
+{
+
+}
