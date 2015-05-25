196f07633001934e2d7e1b84c8528b234349c385
vigonz06 <victorglez96@gmail.com>
2015-05-26 00:16:57 +0200

    ajuste de mails per page

--- fdimail/fdimail/GlobalConstants.h
+++ fdimail/fdimail/GlobalConstants.h
@@ -4,7 +4,7 @@
 
 const int 
 	START_ELEM = 10,
-	MAILS_X_PAGE = 10,
+	MAILS_X_PAGE = 5,
 	MAX_USERS = 20,//N of users
 	MAX_RECIPIENTS = 10,//N of users who can be sent the same mail at once
 	MAX_FASTNAMES = 10,
