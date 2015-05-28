57a417fa8b144d92b6ad453cd2efa0e30b6a1457
vigonz06 <victorglez96@gmail.com>
2015-05-28 23:28:08 +0200

    Revert "Si me dejo algo, dimelo o ponlo tu"
    This reverts commit 6a89919e867d206715fc6cc101f1f2f30554a97b.

--- fdimail/fdimail/fdimail.cpp
+++ fdimail/fdimail/fdimail.cpp
@@ -14,17 +14,15 @@ Version:
 
 Features:
 Apart from the order in the enunciate of the practice, we have implemented:
-	 1. A sistem to interact with the console usig the arrows, and the (ENTER) and (ESCAPE) keys.
-	 2. A submenu to order or filter the active tray by the paramenter you choose.
-	 3. A submenu to change your username, password or delete your account.
-	 4. A sistem of pages for show the active tray, with 5 mails per page.
-	 5. An option to send, or forward a mail to more than one destinatary.
-	 6. A subprogram to encript the user password on the savefile.
-	 7. Detection and correction of errors in data entry console.
-	 8. A submenu to asign alias to other users, or delete them.
-	 9. A subprogram to hide the entry console password.
-	10. An option to forward a mail.
-	
+	1. A submenu to order or filter the active tray by the paramenter you choose.
+	2. A submenu to change your username, password or delete your account.
+	3. An option to send, or forward a mail to more than one destinatary.
+	4. A subprogram to encript the user password on the savefile.
+	5. Detection and correction of errors in data entry console.
+	6. A submenu to asign alias to other users, or delete them.
+	7. A subprogram to hide the entry console password.
+	8. An option to forward a mail.
+
 ---------------------------------*/
 
 #include "Manager.h"
